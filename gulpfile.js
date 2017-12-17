59f2e966af8c8b2a301896a148503766dc312d46
yangyunhan <1205385190@qq.com>
2017-12-17 10:15:32 +0800

    Chore

--- gulpfile.js
+++ gulpfile.js
@@ -1,196 +1,115 @@
-var lr         = require('tiny-lr'),
-    server     = lr(),
-    gulp       = require('gulp'),
-	less       = require('gulp-less'),
-    compass    = require('gulp-compass'),
-    livereload = require('gulp-livereload'),
-    uglify     = require('gulp-uglify'),
-    plumber    = require('gulp-plumber'),
-    webserver  = require('gulp-webserver'),
-    opn        = require('opn'),
-    concat     = require('gulp-concat'),
-    clean      = require('gulp-clean'),
-    imagemin   = require('gulp-imagemin'),
-    pngquant   = require('imagemin-pngquant'),
-    rename     = require("gulp-rename"),
-    zip        = require('gulp-zip'),
-    copy       = require("gulp-copy"),
-    tinypng    = require('gulp-tinypng'),
-    sftp       = require('gulp-sftp'),
-    config     = require('./config.json');
+var gulp = require('gulp');
 
-//压缩javascript 文件，压缩后文件放入build/js下   
-gulp.task('minifyjs',function(){
-    gulp.src('js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./js/min'))
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var gulpSequece = require('gulp-sequence');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var changed = require('gulp-changed');
+var autoprefixer = require('autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var babel = require("gulp-babel");
+var htmlmin = require('gulp-htmlmin');
+var del = require('del');
+var imageMin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var cache = require('gulp-cache');
+
+//删除dist下的所有文件
+gulp.task('delete', function (cb) {
+    return del(['dist/css/*','dist/js/*','dist/*.html','!dist/images'],cb);
 });
 
-//合并build/js文件夹下的所有javascript 文件为一个main.js放入build/js下   
-gulp.task('alljs', function() {
-  return gulp.src('./build/js/*.js')
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest('./js/min'));
+//es6转换为es5
+gulp.task("es6to5", function () {
+    return gulp.src("js/*.js")
+        .pipe(babel({presets: ['env']}))
+        .pipe(gulp.dest("dist/js6"));
+});
+//只压缩js代码
+gulp.task('uglify', function () {
+    setTimeout(function () {
+        gulp.src('dist/js6/*.js')
+            .pipe(uglify())
+            .pipe(gulp.dest('dist/js'))
+    }, 200)
 });
 
-//重命名project.md 文件
-gulp.task('rename', function() {
-  return gulp.src("./Project.md")
-      .pipe(rename("README.md"))
-      .pipe(gulp.dest("./build")); 
+gulp.task('css', function () {  
+    gulp.src(['css/*.css'])  
+        .pipe(changed('dist/css', {hasChanged: changed.compareSha1Digest}))  
+        .pipe(postcss([  
+            autoprefixer({  
+                browsers: ['last 4 version','Android >= 4.0'],//添加浏览器最近的四个版本需要的前缀，兼容安卓4.0以上版本  
+                cascade: false,//是否美化属性,默认true  
+                remove: true//移除不必要的前缀  
+            })]))  
+        .pipe(concat('main.css'))  
+        .pipe(cleanCSS())  
+        .pipe(gulp.dest('dist/css'))  
+        .pipe(browserSync.reload({stream:true}));  
 });
 
-//开启本地 Web 服务器功能
-gulp.task('webserver', function() {
-  gulp.src( './' )
-    .pipe(webserver({
-      host:             config.localserver.host,
-      port:             config.localserver.port,
-      livereload:       true,
-      directoryListing: false
-    }));
+//连接所有js文件并压缩
+gulp.task('scripts',function(){
+	return gulp.src('dist/js6/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(rename('all.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
 });
 
-//通过浏览器打开本地 Web服务器 路径
-gulp.task('openbrowser', function() {
-  opn( 'http://' + config.localserver.host + ':' + config.localserver.port );
-});
-
-//Compass 进行SASS 代码
-gulp.task('compass', function() {
-  gulp.src('./sass/*.scss')
-    .pipe(plumber())
-    .pipe(compass({
-      config_file: './config.rb'
-    }));
-});
-
-//多余文件删除
-gulp.task('clean', function () {
-    return gulp.src('./.sass-cache')
-        .pipe(clean({force: true}))
-        .pipe(gulp.dest('./clean'));
+//压缩html
+gulp.task('htmlmin', function () {
+    var options = {
+        removeComments: true, //清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值<input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值<input id="" />==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面js
+        minifyCSS: true//压缩页面css
+    };
+    gulp.src('index.html')
+        .pipe(changed('dist', {hasChanged: changed.compareSha1Digest}))
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('dist'))
 });
 
 //压缩图片
 gulp.task('imagemin', function () {
-    return gulp.src('img/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('./build/images'));
+    gulp.src('img/*.{png, jpg, gif, ico}')
+        .pipe(cache(imageMin({
+            //optimizationLevel: 5,//类型：Number 默认：3 取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认： false 无损压缩jpg图片
+            //interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行算然
+            //multipass: true, //类型： Boolean 默认： false 多次优化svg直到完全优化
+            svgoPlugins: [{removeViewBox: false}],//不要移除svg的viewbox属性
+            use: [pngquant()]//使用pngquant深度压缩png图片的imagemin插件
+        })))
+        .pipe(gulp.dest('dist/images'));
 });
 
-//压缩图片 - tinypng
-gulp.task('tinypng', function () {
-    gulp.src('img/*.{png,jpg,jpeg}')
-        .pipe(tinypng(config.tinypngapi))
-        .pipe(gulp.dest('./build/images'));
+//Watch Files For Changes
+gulp.task('watch', function(){
+	gulp.watch('js/*.js', ['scripts']);//gulp.watch('js/*.js', ['lint', 'scripts']);
+	gulp.watch('css/*.css',['css']);
+	gulp.watch('index.html',['htmlmin']);
+	//gulp.watch('scss/*.scss',['sass']);
 });
 
-//将相关项目文件复制到build 文件夹下
-gulp.task('buildfiles', function() {
-   //根目录文件
-   gulp.src('./*.{php,html,css,png}')
-   .pipe(gulp.dest('./build'));
-   //CSS文件
-   gulp.src('./css/*')
-   .pipe(gulp.dest('./build/css'));
-    //压缩后的js文件
-   gulp.src('./js/min/*')
-   .pipe(gulp.dest('./build/js'));
+gulp.task('serve', function(){
+	browserSync.init({
+		server: {
+		    baseDir: './'
+        }
+	});
+	gulp.watch(['index.html','css/*.css'],{cwd: './'}, reload);
 });
 
-//文件监控
-gulp.task('watch', function () {
-
-  server.listen(35729, function (err) {
-    if (err){
-      return console.log(err);
-    }
-  });
- 
-  gulp.watch('./sass/*.scss', function (e) {
-    gulp.run('compass');
-    server.changed({
-      body: {
-        files: [e.path]
-      }
-    });
-  });
- 
-  gulp.watch(['./sass/*.scss','./*.html','./*.php','./*.css','./js/*.js'],  function (e) {
-    server.changed({
-      body: {
-        files: [e.path]
-      }
-    });
-  });
- 
-});
-
-//默认任务
-gulp.task('default', function(){
-  console.log('Starting Gulp tasks, enjoy coding!');
-  gulp.run('compass');
-  gulp.run('watch');
-  gulp.run('webserver');
-  gulp.run('openbrowser');
-});
-
-//项目完成提交任务
-gulp.task('build', function(){
-  gulp.run('imagemin');
-  gulp.run('compass');
-  gulp.run('minifyjs');
-  gulp.run('alljs');
-  gulp.run('buildfiles');
-  gulp.run('rename');
-  //gulp.run('clean');
-});
-
-//项目完成提交任务
-gulp.task('build2', function(){
-  gulp.run('tinypng');
-  gulp.run('compass');
-  gulp.run('minifyjs');
-  gulp.run('alljs');
-  gulp.run('buildfiles');
-  gulp.run('rename');
-  //gulp.run('clean');
-});
-
-//打包主体build 文件夹并按照时间重命名
-gulp.task('zip', function(){
-      function checkTime(i) {
-          if (i < 10) {
-              i = "0" + i
-          }
-          return i
-      }
-          
-      var d=new Date();
-      var year=d.getFullYear();
-      var month=checkTime(d.getMonth() + 1);
-      var day=checkTime(d.getDate());
-      var hour=checkTime(d.getHours());
-      var minute=checkTime(d.getMinutes());
-
-  return gulp.src('./build/**')
-        .pipe(zip( config.project+'-'+year+month+day +hour+minute+'.zip'))
-        .pipe(gulp.dest('./'));
-});
-
-//上传到远程服务器任务
-gulp.task('upload', function () {
-    return gulp.src('./build/**')
-        .pipe(sftp({
-            host: config.sftp.host,
-            user: config.sftp.user,
-            port: config.sftp.port,
-            key: config.sftp.key,
-            remotePath: config.sftp.remotePath
-        }));
-});
+//Default Task
+gulp.task('default', ['delete', 'es6to5', 'uglify', 'css', 'htmlmin', 'imagemin', 'watch', 'serve']);
